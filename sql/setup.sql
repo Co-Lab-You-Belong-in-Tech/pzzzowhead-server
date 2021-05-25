90da54326160090a4b3058dc55f24220bcf990a0
Andrew-Bray <amichaelbray@gmail.com>
2021-05-25 12:56:48 -0700

    detailed proof-of-concept

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,1 +1,11 @@
--- put in your SQL database here
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_name TEXT,
+  phone_number VARCHAR(20),
+  wake_up_time VARCHAR(10),
+  sleep_length VARCHAR(10),
+  wind_down_time INTEGER,
+  personality VARCHAR(50)
+);
