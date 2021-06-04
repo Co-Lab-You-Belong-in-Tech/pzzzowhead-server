09f5519df04a21bd73244231e2f82c20e56ec591
Andrew-Bray <amichaelbray@gmail.com>
2021-06-04 14:23:42 -0700

    up and working. No bugs. Time to expand the models

--- sql/setup.sql
+++ sql/setup.sql
@@ -6,9 +6,9 @@ CREATE TABLE users (
   phone_number VARCHAR(20),
   wake_up_time TIME with time zone,
   weekday TEXT ARRAY,
-  sleep_length VARCHAR(50),
+  sleep_length VARCHAR(30),
   bed_Time_alarm TIME with time zone,
-  wind_down_time VARCHAR(10),
+  wind_down_length VARCHAR(30),
   wind_down_alarm TIME with time zone,
-  personality VARCHAR(50)
+  personality VARCHAR(30)
 );
