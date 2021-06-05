2186442bafafddfbbecfba91f6b64f4697d2d0f0
Andrew-Bray <amichaelbray@gmail.com>
2021-06-05 15:20:48 -0700

    3 sms functions in the dictionary, and working as planned

--- data/smsDictionary.js
+++ data/smsDictionary.js
@@ -1,11 +1,20 @@
 const sendSms = require('../lib/utils/SendSms')
 
 const smsDictionary = {
-  windDownSms: function(phoneNumber, theDate, userName, windDownAlarm, cb) {
-    sendSms(phoneNumber, `We sent you a message at this date: ${theDate}! Your name is ${userName}. Your wind down alarm was set for ${cb(windDownAlarm)}`
+  confirmation: 
+    function(phoneNumber, userName, windDownLength, sleepLength, wakeUpTime, cb) {
+      sendSms(phoneNumber, `Hello ${userName}. Welcome to a more rested life! You will receive a reminder to wind down ${windDownLength} before bedtime, sleep for ${sleepLength} hours 😴, and wake up at ${cb(wakeUpTime)}. 🛌🏿 ⏰`)
+    },
+  windDownSms: 
+    function(phoneNumber, theDate, userName, windDownAlarm, cb) {
+      sendSms(phoneNumber, `We sent you a message at this date: ${theDate}! Your name is ${userName}. Your wind down alarm was set for ${cb(windDownAlarm)}`
         )
   },
-  bedTimeSms: 'three'
+  bedTimeSms: 
+    function(phoneNumber, theDate, userName, bedTimeAlarm, cb) {
+    sendSms(phoneNumber, `We sent you a message at this date: ${theDate}! Your name is ${userName}. Your bed time alarm was set for ${cb(bedTimeAlarm)}`
+        )
+  }
 }
 
 module.exports = smsDictionary;
